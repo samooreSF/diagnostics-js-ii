33d57bd1b5288ed90a154f955414d2abe1e039e1
Satchel Moore <samoore@davidson.edu>
2020-02-05 12:06:19 -0800

    All the diagnostics

--- exercises/perfectShuffle.js
+++ exercises/perfectShuffle.js
@@ -9,23 +9,18 @@
 
 function perfectShuffle(array) {
   let arrayLen = array.length;
-  let firstDeck = array.splice(0,arrayLen/2);
-  //console.log(firstDeck);
-  let secondDeck = array.splice(-array.length, arrayLen/2);
-  //console.log(secondDeck);
+  let firstDeck = array.splice(0, arrayLen / 2);
+  // console.log(firstDeck);
+  let secondDeck = array.splice(-array.length, arrayLen / 2);
+  // console.log(secondDeck);
   let join = [];
-  for (let i = 0; i < firstDeck.length; i++){
+  for (let i = 0; i < firstDeck.length; i++) {
     join.push(firstDeck[i]);
     join.push(secondDeck[i]);
-    }
-
+  }
 
   return join;
-
 }
-
-
-
 
 let sanityCheck = require('./sanityCheck');
 
